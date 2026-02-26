629a4935e219e9bab5adc86849bab1fdd2b5dbb6
subham-20000901 <sinhasubh2000@gmail.com>
2026-02-26 12:39:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 //your JS code here. If required.
 const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
+const btn = document.getElementById("btn");
+const error = document.getElementById("error");
+const loading = document.getElementById("loading");
 
 const images = [
   { url: "https://picsum.photos/id/237/200/300" },
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImage(url){
+	return new Promise((resolve,reject) => {
+		const img = new Image();
+		img.src = url;
+		 img.onload = () => resolve(img);
+		img.onerror =() => reject("Failed to load image" + url);
+		
+	});
+}
+
+function downloadImages() {
+	output.innerHTML = "";
+	error.innerText = "";
+	loading.innerText = "Loading...";
+
+	const promises = images.map(image => downloadImage(image.url));
+
+	Promise.all(promises).then((result) => {
+		loading.innerText = "";
+		result.forEach(image => {
+			output.appendChild(image);
+		})
+	}).catch((err) =>{
+		loading.innerText = "";
+		error.innerText = err;
+	})
+	
+}
+btn.addEventListener('click',downloadImages);
